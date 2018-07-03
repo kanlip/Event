54ef0cafc872c0180ec2e1974067930fdf0047f1
sukphasuth Lipipan <sukphasuthlipipan@sukphasuths-MacBook-Pro.local>
2018-07-03 16:57:52 +0700

    A

--- client/app/pages/home.js
+++ client/app/pages/home.js
@@ -56,10 +56,7 @@ class Home extends Component {
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>Madi Event</h1>
                 <h4>
-                  Every landing page needs a small description after the big
-                  bold title, that's why we added this text here. Add here all
-                  the information that can make you or your product create the
-                  first impression.
+                  Book an Event with us on Madi Today
                 </h4>
                 <br />
               </GridItem>
