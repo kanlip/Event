080745964099de6498ef6bcd047bf50b8f928929
sukphasuth Lipipan <sukphasuthlipipan@sukphasuths-MacBook-Pro.local>
2018-07-09 15:23:04 +0700

    update

--- client/app/pages/RenderComponents/renderDetails.js
+++ client/app/pages/RenderComponents/renderDetails.js
@@ -1,69 +1,69 @@
-import withStyles from '@material-ui/core/styles/withStyles';
-import React, { Component } from 'react';
-import Chat from '@material-ui/icons/Chat';
-import CustomTabs from '../../Components/assets/jss/components/CustomTabs/CustomTabs';
-import tabsStyle from '../../Components/assets/jss/material-kit-react/views/componentsSections/tabsStyle';
-import Button from '../../Components/assets/jss/components/CustomButtons/Button';
-import Timestamp from 'grommet/components/Timestamp';
+import withStyles from "@material-ui/core/styles/withStyles";
+import React, { Component } from "react";
+import Chat from "@material-ui/icons/Chat";
+import CustomTabs from "../../Components/assets/jss/components/CustomTabs/CustomTabs";
+import tabsStyle from "../../Components/assets/jss/material-kit-react/views/componentsSections/tabsStyle";
+import Button from "../../Components/assets/jss/components/CustomButtons/Button";
+import Timestamp from "grommet/components/Timestamp";
+import FacebookProvider, { ShareButton } from "react-facebook";
 class RenderDetails extends Component {
   renderTimeStamp() {
     if (this.props.event.startTime !== undefined) {
-      return (
-        <Timestamp value={this.props.event.startTime} />
-      )
-    }
-    else {
-      return (
-        <p>loading....</p>
-      )
+      return <Timestamp value={this.props.event.startTime} />;
+    } else {
+      return <p>loading....</p>;
     }
   }
   render() {
     const { classes } = this.props;
-    const { price, age, address } = this.props.event
+    const { price, age, address } = this.props.event;
     return (
-      <div style={{ padding: '20px 40px 0 0' }}>
+      <div style={{ padding: "20px 40px 0 0" }}>
         <CustomTabs
-          headerColor='info'
+          headerColor="info"
           tabs={[
             {
-              tabName: 'Details',
+              tabName: "Details",
               tabIcon: Chat,
               tabContent: (
-                <span className={classes.textCenter}>
+                <h2
+                  className={classes.textCenter}
+                  style={{ fontWeight: "normal" }}
+                >
                   Minimum Age: {age} <br />
-                  Location : {address}<br />
-                  Time: {this.renderTimeStamp()}<br />
-                  Price: ‎฿ {price===0? 'FREE' : price}
-                </span>
+                  Location : {address}
+                  <br />
+                  Time: {this.renderTimeStamp()}
+                  <br />
+                  Price: ‎฿ <strong>{price === 0 ? "FREE" : price}</strong>
+                </h2>
               )
             }
-
           ]}
         />
-        <div id="share-buttons">
-          <h3 style={{ color: 'black' }}>Share our event:</h3>
-
-          <Button color="facebook" href={`http://www.facebook.com/sharer.php?u=${window.location.href}`}>
-            <i
-              className={'fab fa-facebook-square'}
-            />{' '}
-            Share
-        </Button>
-          <Button href={`https://twitter.com/share?url=${window.location.href}`} color="twitter">
-            <i
-              className={'fab fa-twitter'}
-            />{' '}
-            Share with Twitter
-        </Button>
-          <Button color="google" href={`https://plus.google.com/share?url=${window.location.href}`}>
-            <i
-              className={'fab fa-google-plus-square'}
-            />{' '}
-            Share on Google+
-        </Button>
-
-
+        <div>
+          <h3 style={{ color: "black" }}>Share our event:</h3>
+         
+          <Button
+            color="facebook"
+            href={`http://www.facebook.com/sharer.php?u=${
+              window.location.href
+            }`}
+          >
+            <i className={"fab fa-facebook-square"} /> Share
+          </Button>
+          <Button
+            href={`https://twitter.com/share?url=${window.location.href}`}
+            color="twitter"
+          >
+            <i className={"fab fa-twitter"} /> Share with Twitter
+          </Button>
+          <Button
+            color="google"
+            href={`https://plus.google.com/share?url=${window.location.href}`}
+          >
+            <i className={"fab fa-google-plus-square"} /> Share on Google+
+          </Button>}
         </div>
       </div>
     );
